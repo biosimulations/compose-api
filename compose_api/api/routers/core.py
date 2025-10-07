4d8d87566ad195194e47fc9631905aa4ba6b9b66
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-10-07 08:12:08 -0400

    Separate Routers for API

--- compose_api/api/routers/core.py
+++ compose_api/api/routers/core.py
@@ -21,14 +21,12 @@ from compose_api.dependencies import (
     get_simulation_service,
 )
 from compose_api.simulation.handlers import (
-    get_simulator_versions,
     run_simulation,
 )
 from compose_api.simulation.models import (
     HpcRun,
     JobType,
     PBAllowList,
-    RegisteredSimulators,
     SimulationExperiment,
     SimulationRequest,
 )
@@ -199,18 +197,6 @@ async def get_simulation_status(simulation_id: int = Query(...)) -> HpcRun:
     return await _get_hpc_run_status(ref_id=simulation_id, job_type=JobType.SIMULATION)
 
 
-@config.router.get(
-    path="/simulator/build/status",
-    response_model=HpcRun,
-    operation_id="get-simulator-build-status",
-    tags=["Simulations"],
-    dependencies=[Depends(get_database_service)],
-    summary="Get the simulator build status record by its ID",
-)
-async def get_simulator_build_status(simulator_id: int = Query(...)) -> HpcRun:
-    return await _get_hpc_run_status(ref_id=simulator_id, job_type=JobType.BUILD_CONTAINER)
-
-
 async def _get_hpc_run_status(ref_id: int, job_type: JobType) -> HpcRun:
     db_service = get_database_service()
     if db_service is None:
@@ -227,18 +213,6 @@ async def _get_hpc_run_status(ref_id: int, job_type: JobType) -> HpcRun:
     if simulation_hpcrun is None:
         raise HTTPException(status_code=404, detail=f"{job_type} with id {ref_id} not found.")
     return simulation_hpcrun
-
-
-@config.router.get(
-    path="/simulator/list",
-    response_model=RegisteredSimulators,
-    operation_id="get-simulator-list",
-    tags=["Simulations"],
-    dependencies=[Depends(get_database_service)],
-    summary="Get the list of simulators",
-)
-async def get_simulator_list() -> RegisteredSimulators:
-    return await get_simulator_versions()
 
 
 # @config.router.get(
