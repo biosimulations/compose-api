09ef74f927a52a587f95cb09e728ceefab1c4abb
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-10-16 12:47:46 -0400

    Make Tellurium Clients

--- compose_api/api/client/models/__init__.py
+++ compose_api/api/client/models/__init__.py
@@ -6,6 +6,7 @@ from .bi_graph_step import BiGraphStep
 from .body_analyze_simulation_omex import BodyAnalyzeSimulationOmex
 from .body_run_copasi import BodyRunCopasi
 from .body_run_simulation import BodyRunSimulation
+from .body_run_tellurium import BodyRunTellurium
 from .check_health_health_get_response_check_health_health_get import CheckHealthHealthGetResponseCheckHealthHealthGet
 from .containerization_file_repr import ContainerizationFileRepr
 from .hpc_run import HpcRun
@@ -27,6 +28,7 @@ __all__ = (
     "BodyAnalyzeSimulationOmex",
     "BodyRunCopasi",
     "BodyRunSimulation",
+    "BodyRunTellurium",
     "CheckHealthHealthGetResponseCheckHealthHealthGet",
     "ContainerizationFileRepr",
     "HpcRun",
