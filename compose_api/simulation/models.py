c41188ece9fedddf9486d8344e5f7bc4d643e957
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-02-18 13:46:29 -0500

    Allow For Specific Time Range

--- compose_api/simulation/models.py
+++ compose_api/simulation/models.py
@@ -175,6 +175,7 @@ class SimulationRequest(BaseModel):
 
     request_file_path: Path
     simulation_file_type: SimulationFileType
+    end_time_point: float = 1.0
 
 
 class SimulationResults(BaseModel):
